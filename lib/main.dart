5527e2e22c2b9e51f980ebaa78e3988e6fc321bc
Hudson Bui <hudsonbui369vn@gmail.com>
2025-03-19 14:46:12 +0700

    mod. Change router's position

--- lib/main.dart
+++ lib/main.dart
@@ -2,19 +2,19 @@ import 'package:e_commerce_frontend/scr/core/utils/app_route/app_router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  MyApp({super.key});
-
-  final _router = AppRouter();
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final router = AppRouter();
+
     return MaterialApp.router(
-      routerConfig: _router.config(),
+      routerConfig: router.config(),
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
